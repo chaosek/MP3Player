a236dc99564cfb7eb959441fa47a9d4e3cefb861
chaosek <87547081+chaosek@users.noreply.github.com>
2026-03-14 15:01:26 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE = 'mp3player-v2';
+const CACHE = 'mp3player-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './icons/icon-192-maskable.png',
+  './icons/icon-512-maskable.png'
 ];
 
 self.addEventListener('install', e => {
